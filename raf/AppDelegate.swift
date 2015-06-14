87df9e0a281fdf15c5886f1f42b079ede20e3b61
Andrey Vokin <andrey.vokin@jetbrains.com>
2015-06-15 00:36:21 +0300

    Table view started

--- raf/AppDelegate.swift
+++ raf/AppDelegate.swift
@@ -5,18 +5,25 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow
+    var view: NSView
 
     override init() {
-        var contentSize = NSMakeRect(500.0, 500.0, 1000.0, 1000.0);
+        var contentSize = NSMakeRect(0.0, 0.0, 600.0, 400.0);
         var windowStyleMask = NSTitledWindowMask | NSResizableWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask;
         window = NSWindow(contentRect: contentSize, styleMask:windowStyleMask, backing:NSBackingStoreType.Buffered, defer:true);
 
-        window.backgroundColor = NSColor.whiteColor();
+        window.backgroundColor = NSColor.blueColor();
         window.title = "raf";
+
+        view = NSTableView(frame:CGRectMake(0, 0, 600, 400));
     }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        window.makeKeyAndOrderFront(self);     // Show the window
+        window.makeKeyAndOrderFront(self)
+    }
+
+    func applicationWillFinishLaunching(notification: NSNotification) {
+        window.contentView = view
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
