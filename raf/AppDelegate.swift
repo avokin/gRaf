e1be6d99902d13663217bf46fbb721b3d6a3dc29
Andrey Vokin <andrey.vokin@jetbrains.com>
2015-06-16 10:19:27 +0300

    File list started

--- raf/AppDelegate.swift
+++ raf/AppDelegate.swift
@@ -9,14 +9,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
     var view2: NSTableView
     var splitView: NSSplitView
 
+    var left: String = "/Users/avokin"
+    var right: String = "/"
+
+    var files1 = [String]()
+    var files2 = [String]()
+
     override init() {
         var contentSize = NSMakeRect(0.0, 0.0, 600.0, 400.0);
         var windowStyleMask = NSTitledWindowMask | NSResizableWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask;
-        window = NSWindow(contentRect: contentSize, styleMask:windowStyleMask, backing:NSBackingStoreType.Buffered, defer:true);
+        window = NSWindow(contentRect: contentSize, styleMask: windowStyleMask, backing: NSBackingStoreType.Buffered, defer: true);
         window.title = "raf";
 
-        view1 = NSTableView(frame:CGRectMake(0, 0, 600, 400))
-        view2 = NSTableView(frame:CGRectMake(0, 0, 600, 400))
+        view1 = NSTableView(frame: CGRectMake(0, 0, 600, 400))
+        view2 = NSTableView(frame: CGRectMake(0, 0, 600, 400))
         splitView = NSSplitView(frame: window.frame)
         splitView.vertical = true
         splitView.addSubview(view1)
@@ -28,15 +34,32 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
     }
 
     func createTable(tableView: NSTableView) {
-        tableView.addTableColumn(NSTableColumn(identifier: "FirstName"))
-        tableView.addTableColumn(NSTableColumn(identifier: "LastName"))
+        tableView.addTableColumn(NSTableColumn(identifier: "name"))
+        tableView.addTableColumn(NSTableColumn(identifier: "size"))
         tableView.setDataSource(self);
         tableView.setDelegate(self)
+    }
+
+    func updateTable(path: String, table: NSTableView, inout files: [String]) {
+        let fileManager = NSFileManager.defaultManager()
+        files.removeAll(keepCapacity: false)
+
+        var allFiles = fileManager.contentsOfDirectoryAtPath(path, error: nil)
+        var allSuperFiles = allFiles as! [String]
+        for element: String in allSuperFiles {
+            println(element)
+            files.append(element)
+        }
+
+        table.reloadData()
     }
 
     func applicationWillFinishLaunching(notification: NSNotification) {
         createTable(view1)
         createTable(view2)
+
+        updateTable(left, table: view1, files: &files1)
+        updateTable(right, table: view2, files: &files2)
 
         window.contentView = splitView
     }
@@ -45,24 +68,19 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
         return true;
     }
 
-    func numberOfRowsInTableView(tableView: NSTableView) -> Int
-    {
-        let numberOfRows:Int = getDataArray().count
-        return numberOfRows
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        var files = files1
+        if (tableView == view2) {
+            files = files2
+        }
+        return files.count
     }
 
-    func getDataArray () -> NSArray{
-        var dataArray:[NSDictionary] = [["FirstName": "Debasis", "LastName": "Das"],
-                                        ["FirstName": "Nishant", "LastName": "Singh"],
-                                        ["FirstName": "John", "LastName": "Doe"],
-                                        ["FirstName": "Jane", "LastName": "Doe"],
-                                        ["FirstName": "Mary", "LastName": "Jane"]];
-        return dataArray;
-    }
-
-    func tableView(tableView: NSTableView!, objectValueForTableColumn tableColumn: NSTableColumn!, row: Int) -> AnyObject!
-    {
-        var newString = getDataArray().objectAtIndex(row).objectForKey(tableColumn.identifier)
-        return newString;
+    func tableView(tableView: NSTableView!, objectValueForTableColumn tableColumn: NSTableColumn!, row: Int) -> AnyObject! {
+        var files = files1
+        if (tableView == view2) {
+            files = files2
+        }
+        return files[row]
     }
 }
