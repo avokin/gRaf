cdbae58026852f5c48b6bd26ddf2236e5c04ce06
Andrey Vokin <andrey.vokin@jetbrains.com>
2015-07-14 14:06:44 +0500

    PaneModel started

---
+++ raf/Model/PaneModel.swift
@@ -0,0 +1,12 @@
+//
+// Created by Andrey Vokin on 14/07/15.
+// Copyright (c) 2015 Andrey Vokin. All rights reserved.
+//
+
+import Foundation
+
+class PaneModel {
+    var path: String = "/"
+    var sortBy: SortBy = .Name
+    var sortDirection: SortDirection = .Ascending
+}
---
+++ raf/Model/Sorting.swift
@@ -0,0 +1,12 @@
+enum SortBy {
+    case Name
+    case Extension
+    case Size
+    case DateModified
+}
+
+enum SortDirection {
+    case Ascending
+    case Descending
+}
+
