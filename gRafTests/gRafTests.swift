194c68c2d9b047d221e1151f0370b397c6e463ec
Andrey Vokin <andrey.vokin@gmail.com>
2016-01-31 18:27:06 +0300

    Failed test added

--- gRafTests/gRafTests.swift
+++ gRafTests/gRafTests.swift
@@ -23,7 +23,7 @@ class gRafTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        XCTAssert(false, "Failed")
     }
     
     func testPerformanceExample() {
